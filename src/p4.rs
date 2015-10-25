6d1d37bc1ed32e844c357bf384ad13fe41a795e2
haywood <mhr2126@gmail.com>
2015-10-25 11:03:05 -0700

    solution to p4

---
+++ src/p4.rs
@@ -0,0 +1,56 @@
+pub fn solve() -> i64 {
+    return largest_palindrome_product(3);
+}
+
+pub fn largest_palindrome_product(n: i16) -> i64 {
+    if n < 1 {
+        panic!("invalid input for largest_palindrome_product: {}", n);
+    }
+    let mut max_str = String::new();
+    for _ in 0..n {
+        max_str.push('9');
+    }
+    let max = max_str.parse::<i64>().unwrap();
+    let mut min_str = "1".to_string();
+    for _ in 0..n-1 {
+        min_str.push('0');
+    }
+    let min = min_str.parse::<i64>().unwrap();
+    for i in (min..max + 1).rev() {
+        for j in (min..i).rev() {
+            let p = i * j;
+            if is_palindrome(p) {
+                return p;
+            }
+        }
+    }
+    panic!("unable to find a palindrome product of {} digit numbers", n);
+}
+
+pub fn is_palindrome(n: i64) -> bool {
+    let str = n.to_string();
+    let bytes = str.as_bytes();
+    let len = bytes.len();
+    let mid = len / 2;
+    for i in 0..mid {
+        if bytes[i] != bytes[len - i - 1] {
+            return false;
+        }
+    }
+    return true;
+}
+
+#[test]
+fn test_solve() {
+    assert_eq!(solve(), 580085);
+}
+
+#[test]
+fn test_largest_palindrome_product() {
+    assert_eq!(largest_palindrome_product(2), 9009);
+}
+
+#[test]
+fn test_is_palindrome() {
+    assert!(is_palindrome(9009));
+}
